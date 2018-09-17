b0c8641fe444a17a830304940685e673545fa5fa
Pulya10c <sergienya.oleg@yandex.ru>
2018-09-18 00:42:00 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,26 +1,58 @@
 class Sorter {
   constructor() {
     // your implementation
+   this.MyArray = [];
+      
   }
+
+
 
   add(element) {
     // your implementation
+    return this.MyArray.push(element);
   }
 
   at(index) {
     // your implementation
+    return this.MyArray[index];
   }
 
   get length() {
     // your implementation
+    return this.MyArray.length;
   }
 
   toArray() {
     // your implementation
+    return this.MyArray;
   }
 
   sort(indices) {
     // your implementation
+    function comparator(a, b) {
+      if (a < b ) {
+        return -1;
+      }
+      if (a > b ) {
+        return 1;
+      }
+        return 0;
+    }
+indices.sort(comparator);
+testArray = [];
+for (i=0;i<=indices.length;i++) {
+testArray[i] = this.MyArray[indices[i]];
+}
+testArray.sort(comparator);
+for (i=0;i<=indices.length;i++) {
+  this.MyArray[indices[i]]=testArray[i];
+  }
+  return this.MyArray;
+
+    //for (i=this.MyArray.length;i>=0;i--) {
+
+}
+
   }
 
   setComparator(compareFunction) {
